5c81e8f098003255300e24962f9f40f28ffca7f7
Alex Morega <alex@grep.ro>
2014-07-02 16:40:31 +0300

    scrape data from feeds

--- app/extensions/AptiScraper/extension.php
+++ app/extensions/AptiScraper/extension.php
@@ -68,10 +68,97 @@ class Controller
         return $prefix . "aptiscraper_visited";
     }
 
-    public function view(Silex\Application $app, Request $request) {
+    public function fetch_feed($url) {
+        $cache_path = $this->app['resources']->getPath('cache') . '/simplepie';
+        if (!file_exists($cache_path)) {
+            mkdir($cache_path, 0777, true);
+        }
+
+        $feed = new \SimplePie();
+        $feed->set_cache_location($cache_path);
+        $feed->set_feed_url($url);
+        $feed->init();
+        $feed->handle_content_type();
+
+        echo "feed title: " . $feed->get_title();
+
+        $rv = array();
+
+        foreach ($feed->get_items() as $item) {
+            $rv[] = array(
+                'key' => $url . " ~~~ " . $item->get_id(),
+                'title' => $item->get_title(),
+                'description' => $item->get_description(),
+                'date' => $item->get_date('j F Y | g:i a'),
+            );
+        }
+
+        return $rv;
+    }
+
+    public function visit_item($item) {
+        $table_name = $this->get_table_name();
+        $rv = false;
+
         $db = $this->app['db'];
-        $res = $db->insert($this->get_table_name(), array('key' => "foo"));
-        return "hello <em>scraper</em>!";
+        $db->beginTransaction();
+        try {
+            $stmt = $db->prepare(
+                'SELECT count(*) as count FROM ' . $table_name .
+                ' WHERE key = ?'
+            );
+            $stmt->bindValue(1, $item['key']);
+            $stmt->execute();
+            $seen = $stmt->fetchAll()[0]['count'] > 0;
+
+            if(! $seen) {
+                $db->insert($table_name, array('key' => $item['key']));
+                $this->create_cms_item($item);
+                $rv = true;
+            }
+
+            $db->commit();
+        } catch(Exception $e) {
+            $db->rollback();
+            throw $e;
+        }
+
+        return $rv;
+    }
+
+    public function create_cms_item($item) {
+    }
+
+    public function scrape_feed($url) {
+        $count = 0;
+        foreach ($this->fetch_feed($url) as $item) {
+            if($this->visit_item($item)) {
+                $count += 1;
+            }
+        }
+        return $count;
+    }
+
+    public function scrape_all() {
+        $url_list = array(
+            'https://medium.com/feed/message',
+        );
+
+        $rv = array();
+        foreach($url_list as $url) {
+            $count = $this->scrape_feed($url);
+            $rv[] = array("url" => $url, "count" => $count);
+        }
+        return $rv;
+    }
+
+    public function view(Silex\Application $app, Request $request) {
+        $report = "";
+        foreach($this->scrape_all() as $feed) {
+            $report .= "" . $feed['count'] . " " . $feed['url'] . "\n";
+        }
+        ;
+        return "<code><pre>" . $report . "</code></pre>\n";
     }
 
 }
